d26fa3f16341880246ee0321c5fad66455f7d358
Richard Carlsson <richardc@klarna.com>
2011-09-19 21:25:30 +0200

    removed duplicated code

--- src/eg_lib.erl
+++ src/eg_lib.erl
@@ -19,72 +19,15 @@
 %% USE OR OTHER DEALINGS IN THE SOFTWARE.
 %%==========================================================================
 
+%% shared internal functions
+
 -module(eg_lib).
 
--export([showGrid/2, moveAndShow/4, find_files/3, find_files/5,
+-export([find_files/3, find_files/5,
          priv_dir/0, priv_src_dir/0]).
 
 -include_lib("kernel/include/file.hrl").
 
-
-%% showGrid(PDF, a4 | usLetter) 
-%%   adds a grid to the current page page
-
-showGrid(PDF, Paper) ->
-    {PaperWidth, PaperHeight} = sizeOfPaper(Paper),
-    %% Top = PaperHeight - 10,
-    Top = 825, % hack
-    Bottom = 10,
-    Left = 10,
-    %% Right = PaperWidth - 10,
-    Right = 575,
-    eg_pdf:set_font(PDF,"Helvetica", 8),
-    vlines(PDF, Left, Right, Top, Bottom),
-    hlines(PDF, Left, Right, Top, Bottom).
-
-sizeOfPaper(a4) ->
-    {595, 842};
-sizeOfPaper(usLetter) ->
-    {612, 792}.
-
-hlines(PDF, Left, Right, Top, Bottom) ->
-    diter(Top,25,10,
-	  fun(Y) ->
-		  %% eg_pdf:set_fill_gray(PDF,1.0),
-		  eg_pdf:line(PDF, Left, Y, Left+20, Y),
-		  eg_pdf:line(PDF, Right, Y, Right-20, Y),
-		  %% eg_pdf:set_fill_gray(PDF,0.8),
-		  eg_pdf:line(PDF, Left+20,Y,Right-20,Y),
-		  moveAndShow(PDF, Left, Y+2, eg_pdf_op:n2s(Y)),
-		  moveAndShow(PDF, Right-20, Y+2, eg_pdf_op:n2s(Y)),
-		  true
-	  end).
-
-vlines(PDF, Left, Right, Top, Bottom) ->
-    diter(Right,25,10,
-	  fun(X) ->
-		  eg_pdf:line(PDF, X, Top, X, Top-20),
-		  moveAndShow(PDF, X-5, Top-35,eg_pdf_op:n2s(X)),
-		  eg_pdf:line(PDF, X, Bottom, X, Bottom+20),
-		  eg_pdf:line(PDF, X, Top -40, X, Bottom + 35),
-		  moveAndShow(PDF, X-5, Bottom+23,eg_pdf_op:n2s(X))
-	  end).
-
-moveAndShow(PDF, X, Y, Str) ->
-    eg_pdf:begin_text(PDF),
-    eg_pdf:set_text_pos(PDF, X, Y),
-    eg_pdf:text(PDF, Str),
-    eg_pdf:end_text(PDF).
-
-%% downwards iterator
-
-diter(X, Inc, Stop, F) when X < Stop ->
-    true;
-diter(X, Inc, Stop, F) ->
-    F(X), diter(X-Inc,Inc,Stop,F).
-    
-
-%%==========================================================================
 
 %% Examples:
 %%   find_files(".", "*.erl", false)
